86ab08e76065c55029befcb1a455e55ad67bf1c8
Alvaro Garcia <alvaro.gargon.4@ED49000391P303.educa.jcyl.es>
2025-03-28 09:16:32 +0100

    finalizacion binarios

--- UD08A02_archivos_binario/src/main/java/es/sauces/ejercico4/ud08a02_archivos_binario/Ej8.java
+++ UD08A02_archivos_binario/src/main/java/es/sauces/ejercico4/ud08a02_archivos_binario/Ej8.java
@@ -13,6 +13,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,9 +28,25 @@ public class Ej8 {
      * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException {
-        short ancho = 0, alto = 0;
-        byte [][] imagen = null;
-        File fich = new File("barbara_512x512.raw");
+        System.out.println("Introduzca nombre imagen: ");
+        Scanner teclado = new Scanner(System.in);
+        String nombreArchivo;
+        byte [][] imagen=null;
+        nombreArchivo=teclado.nextLine();
+        
+        try{
+            imagen=leerArchivoImagen(nombreArchivo);
+            imagen=crearNegativo(imagen);// esto rompe la imagen original
+            imagen=espejoHorizontal(imagen);
+            escribirArchivoImagen(nombreArchivo+"-copia",imagen);
+        }catch(ImagenException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    private static byte[][] leerArchivoImagen(String nombre) throws FileNotFoundException, ImagenException{
+        short ancho,alto;
+        byte [][] imagen=null;
+        File fich = new File(nombre+".raw");
         FileInputStream fis = new FileInputStream(fich);
         BufferedInputStream bis = new BufferedInputStream(fis);
         try (DataInputStream entrada = new DataInputStream(bis); 
@@ -43,9 +60,15 @@ public class Ej8 {
                     }
                 }
         } catch (IOException ex) {
-            Logger.getLogger(Ej8.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ImagenException(ex.toString());
         }
-        try(DataOutputStream salida = new DataOutputStream(Files.newOutputStream(Paths.get("barbara-copia_512x512.raw")))){
+        return imagen;
+    }
+    private static void escribirArchivoImagen(String nombre, byte[][] imagen) throws ImagenException{
+        short ancho = 0,alto = 0;
+        alto=(short) imagen.length;
+        ancho=(short) imagen[0].length;
+        try(DataOutputStream salida = new DataOutputStream(Files.newOutputStream(Paths.get(nombre+".raw")))){
             salida.writeShort(ancho);
             salida.writeShort(alto);
             for (int i = 0; i < alto; i++) {
@@ -54,9 +77,63 @@ public class Ej8 {
                     }
                 }
         } catch (IOException ex) {
-            Logger.getLogger(Ej8.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ImagenException(ex.toString());
         }
-        
     }
-
+    
+    private static byte[][] crearNegativo(byte[][] imagen){
+        byte[][] nuevaImagen;
+        short ancho,alto;
+        alto=(short) imagen.length;
+        ancho=(short) imagen[0].length;
+        nuevaImagen=new byte[alto][ancho];
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j < ancho; j++) {
+                nuevaImagen[i][j]=(byte) (255-imagen[i][j]);
+            }
+        }
+        return nuevaImagen;
+    }
+    
+    private static byte[][] espejoHorizontal(byte[][] imagen){
+        byte[][] nuevaImagen;
+        short ancho,alto;
+        alto=(short) imagen.length;
+        ancho=(short) imagen[0].length;
+        nuevaImagen=new byte[alto][ancho];
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0, j2=ancho-1; j < ancho; j++,j2--) {
+                nuevaImagen[i][j2]= imagen[i][j];
+            }
+        }
+        return nuevaImagen;
+    }
+    
+    private static byte[][] rotarImagenIzquierda(byte[][] imagen){
+        byte[][] nuevaImagen;
+        short ancho,alto;
+        alto=(short) imagen.length;
+        ancho=(short) imagen[0].length;
+        nuevaImagen=new byte[ancho][alto];//cambian las diemnsiones de la matriz
+        for (int i = 0,j2=0; i < alto; i++,j2++) {
+            for (int j = 0,i2=ancho-1; j < ancho; j++,i2--) {
+                nuevaImagen[i2][j2]= imagen[i][j];
+            }
+        }
+        return nuevaImagen;
+    }
+    
+    private static byte[][] rotarImagenDerecha(byte[][] imagen){
+        byte[][] nuevaImagen;
+        short ancho,alto;
+        alto=(short) imagen.length;
+        ancho=(short) imagen[0].length;
+        nuevaImagen=new byte[ancho][alto];//cambian las diemnsiones de la matriz
+        for (int i = 0,j2=alto-1; i < alto; i++,j2--) {
+            for (int j = 0, i2=0; j < ancho; j++,i2++) {
+                nuevaImagen[i2][j2]= imagen[i][j];
+            }
+        }
+        return nuevaImagen;
+    }
 }
---
+++ UD08A02_archivos_binario/src/main/java/es/sauces/ejercico4/ud08a02_archivos_binario/ImagenException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.sauces.ejercico4.ud08a02_archivos_binario;
+
+/**
+ *
+ * @author alvaro.gargon.4
+ */
+public class ImagenException extends Exception{
+    public ImagenException(String mensaje){
+        super(mensaje);
+    }
+}
