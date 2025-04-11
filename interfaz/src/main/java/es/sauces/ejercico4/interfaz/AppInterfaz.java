9852eb13d9f068cd4b27117ba386d80b9156e211
Alvaro Garcia <alvaro.gargon.4@ED49000391P303.educa.jcyl.es>
2025-04-11 10:09:27 +0200

    Agenda Vista

---
+++ interfaz/src/main/java/es/sauces/ejercico4/interfaz/AppInterfaz.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package es.sauces.ejercico4.interfaz;
+
+/**
+ *
+ * @author alvaro.gargon.4
+ */
+public class AppInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Window interfaz=new Window();
+        interfaz.setSize(300, 200);
+        interfaz.setVisible(true);
+    }
+    
+}
